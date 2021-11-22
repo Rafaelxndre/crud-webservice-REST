d2c1846ec52e5126f819be00a7452d2a291eb21c
Rafaelxndre <rafaelxndre@gmail.com>
2021-11-22 19:38:02 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/springcrud/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/springcrud/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.springcrud.entities.Client;
 import com.devsuperior.springcrud.repositories.ClientRepository;
@@ -14,6 +15,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional
 	public List<Client> findAll(){
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Rafael Alexandre', '1597846823', 17500.00, TIMESTAMP WITH TIME ZONE '2015-08-22T20:59:14Z', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Guilherme Moreira', '1563241587', 102472.00, TIMESTAMP WITH TIME ZONE '2012-03-15T20:16:16Z', 3);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Felipe Oliveira', '4785146328', 8400.00, TIMESTAMP WITH TIME ZONE '2018-06-12T20:23:19Z', 5);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Gabriel Mendes', '6521423879', 13000.00, TIMESTAMP WITH TIME ZONE '2016-01-14T20:45:12Z', 1);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Joao Lucas', '5641238745', 4000.00, TIMESTAMP WITH TIME ZONE '2008-04-13T20:56:11Z', 1);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Teodoro Fonseca', '8452167846', 8500.00, TIMESTAMP WITH TIME ZONE '2014-06-03T14:56:11Z', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Jorge Montero', '5478218964', 4700.00, TIMESTAMP WITH TIME ZONE '2013-07-19T21:34:09Z', 6);
