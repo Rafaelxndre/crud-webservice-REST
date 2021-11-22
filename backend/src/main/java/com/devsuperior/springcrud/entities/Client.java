6a79bdc71a6f3596637f1adcb067c6c485d62eba
Rafaelxndre <rafaelxndre@gmail.com>
2021-11-22 15:29:26 -0300

    ClientResource

---
+++ backend/src/main/java/com/devsuperior/springcrud/entities/Client.java
@@ -0,0 +1,96 @@
+package com.devsuperior.springcrud.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+public class Client implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+
+	public Client() {
+		
+	}
+
+	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}	
+}
