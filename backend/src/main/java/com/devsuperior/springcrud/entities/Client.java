5f4cd223ac1d1d44465217077804700b8406320d
Rafaelxndre <rafaelxndre@gmail.com>
2021-11-24 12:56:18 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/springcrud/entities/Client.java
+++ backend/src/main/java/com/devsuperior/springcrud/entities/Client.java
@@ -4,10 +4,13 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +26,12 @@ public class Client implements Serializable{
 	private Double income;
 	private Instant birthDate;
 	private Integer children;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 
 	public Client() {
 		
@@ -84,6 +93,24 @@ public class Client implements Serializable{
 
 	public void setChildren(Integer children) {
 		this.children = children;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
